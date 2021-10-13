5329c62b99f638d5509b241a6c623e71cbdadcf1
Lokostyk <lokostyk12@gmail.com>
2021-10-13 22:38:34 +0200

    Added

---
+++ src/Components/Main.js
@@ -0,0 +1,30 @@
+import React,{useEffect,useRef} from 'react'
+import gsap from 'gsap'
+
+export default function Main() {
+    let main = useRef(null)
+
+    useEffect(()=>{
+        const timeLine = gsap.timeline({defaults:{duration:1}})
+        timeLine
+            .from(main.querySelector(".mainTitle"),{opacity:0},.4)
+            .from(main.querySelectorAll(".mainTxt"),{height:0})
+            .to(main.querySelector(".mainBtn"),{padding: "0.5rem 3.5rem",maxWidth: "400px"})
+            .to(main.querySelector(".mainBtn"),{border: "6px solid white",duration:.1})
+    },[])
+
+    return (
+        <main ref={el=>main=el} className="mainBackground">
+            <div className="mainContent">
+                <p className="mainTitle">Go <span>&nbsp;out</span>door</p>
+                <div></div>
+                <div>
+                    <p className="mainTxt">with <span>Confidence</span></p>
+                    <p className="mainTxt">and <span>Comfort</span></p>
+                    <button className="mainBtn">explore</button>
+                </div>
+            </div>
+            <img className="backgroundImage" src="/pictures/mountains1.jpg" alt="mountains"/>
+        </main>
+    )
+}
