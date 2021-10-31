22306909db25bf4a9fa5c99606313047c3664df7
Lokostyk <lokostyk12@gmail.com>
2021-10-31 21:00:30 +0100

    Different colors added

--- src/Components/ProductPage/Product.js
+++ src/Components/ProductPage/Product.js
@@ -8,11 +8,11 @@ export default function Product(props) {
     const {pathname} = useLocation()
     const id = Number(pathname.slice(pathname.lastIndexOf("/")+1))
     const productData = useSelector((state)=>state.bestSellers.filter(item=>item.id === id)[0])
-    const [currentImg,setCurrentImg] = useState("/pictures/jacket.jpg")
+    const [currentImg,setCurrentImg] = useState("/pictures/polar-black.jpg")
 
     useEffect(()=>{
         gsap.from(img,{opacity:0,duration:1})
-    },[img,currentImg])
+    },[])
     return (
         <div className="productContainer">
             <div className="product">
@@ -21,7 +21,12 @@ export default function Product(props) {
                     <h1>{productData.name}</h1>
                     <p>{productData.price}<span style={{color:"#e6a519"}}>$</span></p>
                     <p style={{fontWeight:"bold"}}>Colors</p>
-                    <input type="radio" />
+                    <form>
+                        {productData.colors.map((item,index)=>{
+                            return (<input type="radio" name="colors" onClick={()=>setCurrentImg(productData.imgs[index])} style={{backgroundColor:item}}/>)
+                        })}
+                    
+                    </form>
                     <button className="addBtn" onClick={()=>setCurrentImg("/pictures/trousers.jpg")}>Add to Bag</button>
                 </div>
             </div>
--- src/Redux/shopReducers.js
+++ src/Redux/shopReducers.js
@@ -1,4 +1,4 @@
-const bestSellersState = {bestSellers:[{id:1,name:"warm booster",colors:["black","brown"],imgs:["pictures/polar-black.jpg","pictures/polar-brown.jpg"],price:"49.99",category:"jackets",gender:"male"}]}
+const bestSellersState = {bestSellers:[{id:1,name:"Ultra warm booster",colors:["black","brown"],imgs:["/pictures/polar-black.jpg","/pictures/polar-brown.jpg"],price:"49.99",category:"jackets",gender:"male"}]}
 
 export const bestSellersReducer = (state = bestSellersState,action)=>{
     switch(action.type){
