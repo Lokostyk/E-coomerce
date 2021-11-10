00996caedcb1053eab3c6db1cea471091af7b51d
Lokostyk <lokostyk12@gmail.com>
2021-11-10 23:16:51 +0100

    Added mobile support

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,19 +1,35 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import {Link} from "react-router-dom"
 import { useSelector } from "react-redux"
 
 export default function Navbar() {
     const basketItemCount = useSelector((state)=>state.basket.contents.length)
+    const [mobileSidebar,setMobileSidebar] = useState(false)
 
+    useEffect(()=>{
+        const navbarOverflow = document.querySelector(".linksContainer").style
+        if(mobileSidebar){
+            setTimeout(()=>{
+                navbarOverflow.overflow = "visible"
+            },300)
+            window.addEventListener("click",closeSidebar)
+        }else {
+            navbarOverflow.overflow = "hidden"
+        }
+    },[mobileSidebar])
+    const closeSidebar = ()=>{
+        setMobileSidebar(false)
+        window.removeEventListener("click",closeSidebar)
+    }
     return (
-        <nav className="navbar">
+        <nav className={`navbar ${mobileSidebar?"activeNavbar":""}`}>
             <Link className="logo" to="/">
                 <img src="/pictures/logo.png" />
                 <div/>
                 <p>JourneyEmpire</p>
             </Link>
-            <button className="hamburgerBtn">awdwad</button>
-            <ul>
+            <button className="hamburgerBtn" onClick={()=>setMobileSidebar(true)}><img src="/pictures/hamburger.svg"/></button>
+            <ul className="linksContainer">
                 <li>
                     <ul className="products">
                         <Link to="/products#all" className="navLink" >Products</Link>
@@ -28,6 +44,7 @@ export default function Navbar() {
                 <li><Link to="/#aboutUs" className="navLink">About Us</Link></li>
                 <li><Link to="/#contactContainer" className="navLink">Contact</Link></li>
                 <li><Link to="/" className="navLink">Info</Link></li>
+                <button onClick={()=>setMobileSidebar(false)}>dawd</button>
             </ul>
             <Link to="/basket" className="cart">
                 <img src="/pictures/shopping-bag.png"/>
